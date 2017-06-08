9ad0f61f8ac67fdda1139ace34c04d2cf2f8b224
Nathan <natethepcspecialist@gmail.com>
2017-06-08 14:32:00 -0400

    update

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var express = require('express'),
 app.use(express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/views'));
+var file = fs.readFileSync('./bower_components/angular/angular.js', 'utf-8');
+console.log(file);
 
 server.listen(process.env.PORT || 3000, function () {
     console.log('Listening on port 3000');
